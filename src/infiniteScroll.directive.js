4d2d67406975316b2c712df4749d1e8dc140eb50
Marlboro11526 <RichardJohnston33@outlook.com>
2017-09-23 12:46:32 -0700

    refactor

--- src/infiniteScroll.directive.js
+++ src/infiniteScroll.directive.js
@@ -3,13 +3,18 @@ const infiniteScroll = () => {
     restrict: 'A',
     link(scope, element, attrs) {
       const raw = element[0];
+      let options = {
+        so: 5,
+        sho: 200,
+        m: (typeof window.orientation !== 'undefined')
+      }
       const so = attrs.scrollOffset;
       const sho = attrs.scrollShOffset;
-      const offset =  so ? parseInt(so) : 5;
-      const scrollHeightOffset = sho ? parseInt(sho) : 200;
-      let m = attrs.isMobile;
-      if (!m && typeof window.orientation !== 'undefined') m = true;
-      const scrollEvt = m === true ? 'scroll' : 'mousewheel';
+      const m = attrs.isMobile;
+      if (so) options.so =  parseInt(so);
+      if (sho) options.sho = parseInt(sho);
+      if (m) options.m = m;
+      const scrollEvt = options.m === true ? 'scroll' : 'mousewheel';
 
       if (attrs.scrollUnbind) {
         scope.$watch(attrs.scrollUnbind, function(val) {
@@ -22,7 +27,7 @@ const infiniteScroll = () => {
       // apply infiniteScroll fn when scrolled past limit
       var onScroll = function(evt) {
         if (scope.scrollLoading) return;
-        if ((raw.scrollTop + raw.offsetHeight + offset) >= (raw.scrollHeight + scrollHeightOffset)) {
+        if ((raw.scrollTop + raw.offsetHeight + options.so) >= (raw.scrollHeight + options.sho)) {
           scope.scrollLoading = true;
           scope.$apply(attrs.alInfiniteScroll);
           evt.preventDefault();
